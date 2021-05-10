63ee80633489c92056d7b6112c5a7e5f10718e8b
First Last <email.mail.com>
2021-05-10 02:27:25 -0500

    first commit

---
+++ src/main/java/com/clouddottech/microserviceGradleJAR/MicroserviceGradleJarApplication.java
@@ -0,0 +1,13 @@
+package com.clouddottech.microserviceGradleJAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGradleJarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceGradleJarApplication.class, args);
+	}
+
+}
